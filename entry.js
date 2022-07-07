5ee13d4d79f46299589ec5f4c76525b22519e109
mqzhifu <mqzhifu@sina.com>
2022-07-07 13:19:44 +0800

    dddd

--- entry.js
+++ entry.js
@@ -3,9 +3,9 @@ import * as Control from './Control'
 //入口文件
 
 async function startBasicCall() {
-    Control.initPre();//脚本 前置 - 初始化
     //页面载完成
     window.onload = function () {
+        Control.initPre();//脚本 前置 - 初始化
         Control.initRtc();//初始化RTC
         Control.initRtm();//初始化RTM
     };
